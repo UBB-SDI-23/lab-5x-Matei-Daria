74e805ddc519ece7a9cc57a83c8a4c7ebd76e374
Matei-Daria <91487802+Matei-Daria@users.noreply.github.com>
2023-04-10 14:53:04 +0300

    Update constants.ts

--- react-frontend/src/constants.ts
+++ react-frontend/src/constants.ts
@@ -1,12 +1,12 @@
+// const PROD_BACKEND_API_URL = "/api";
+// const DEV_BACKEND_API_URL = "http://127.0.0.1:8000/api";
+
+// export const BACKEND_API_URL =
+// 	process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;
+
+
 const PROD_BACKEND_API_URL = "/api";
-const DEV_BACKEND_API_URL = "http://127.0.0.1:8000/api";
+const DEV_BACKEND_API_URL = "http://ec2-16-170-241-120.eu-north-1.compute.amazonaws.com/api";
 
 export const BACKEND_API_URL =
 	process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;
-
-
-// const PROD_BACKEND_API_URL = "/api";
-// const DEV_BACKEND_API_URL = "http://ec2-16-170-241-120.eu-north-1.compute.amazonaws.com/api";
-//
-// export const BACKEND_API_URL =
-// 	process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;
